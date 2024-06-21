f9ea4b2cf76e0fbe548a26fded935b2b2324ad0e
kent vicente <125102954+Eggy07@users.noreply.github.com>
2024-06-21 22:06:49 +0800

    register_login_okay_na_unta

--- front_end/src/components/LoginForm.jsx
+++ front_end/src/components/LoginForm.jsx
@@ -1,138 +1,170 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axiosInstance from '../axios';
 import { useAuth } from '../context/AuthContext';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
-import { login, getUser, getCsrfToken} from '../auth';
+import { login, getUser, getCsrfToken } from '../auth';
+import './animations.css';
 
 const LoginForm = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-    const [isEmailFocused, setIsEmailFocused] = useState(false);
-    const [isPasswordFocused, setIsPasswordFocused] = useState(false);
-    const navigate = useNavigate();
-    const { handleLogin } = useAuth();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [isEmailFocused, setIsEmailFocused] = useState(false);
+  const [isPasswordFocused, setIsPasswordFocused] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const { handleLogin } = useAuth();
 
-    useEffect(() => {
-        async function fetchCsrfToken() {
-            try {
-                const { data } = await axiosInstance.get('/csrf-token');
-                localStorage.removeItem('authToken');
-                localStorage.removeItem('studentName');
-                localStorage.removeItem('userRole');
-                console.log(data.csrf_token);
-                axiosInstance.defaults.headers['X-CSRF-TOKEN'] = data.csrf_token;
-            } catch (error) {
-                console.error('Error fetching CSRF token:', error);
-                setError('Failed to fetch CSRF token.');
-            }
-        }
-        fetchCsrfToken();
-    }, []);
-
-    const getToken = async () => {
+  useEffect(() => {
+    async function fetchCsrfToken() {
         try {
-          const token = await getCsrfToken();
-          console.log(token);
-          return token;
+            const { data } = await axiosInstance.get('/csrf-token');
+            localStorage.removeItem('authToken');
+            localStorage.removeItem('studentName');
+            localStorage.removeItem('userRole');
+            console.log(data.csrf_token);
+            axiosInstance.defaults.headers['X-CSRF-TOKEN'] = data.csrf_token;
         } catch (error) {
-          console.error('Error fetching CSRF token:', error);
-          setError('Failed to fetch CSRF token.');
+            console.error('Error fetching CSRF token:', error);
+            setError('Failed to fetch CSRF token.');
         }
-    };
+    }
+    fetchCsrfToken();
+  }, []);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setError('');
+  const getToken = async () => {
+    try {
+      const token = await getCsrfToken();
+      console.log(token);
+      return token;
+    } catch (error) {
+      console.error('Error fetching CSRF token:', error);
+      setError('Failed to fetch CSRF token.');
+    }
+  };
 
-        try {
-          const token = getToken();
-          console.log(token);
-          await login(email, password);
-          localStorage.setItem('authToken', token);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    setIsLoading(true);
 
-          const userData = await getUser(); 
-          console.log(userData.role);
+    try {
+      const token = await getToken();
+      console.log(token);
+      await login(email, password);
+      localStorage.setItem('authToken', token);
 
-          handleLogin(userData);
-          if (userData.role === 'admin') {
-            navigate('/admin/dashboard');
-          } else if (userData.role === 'student') {
-            navigate('/student/dashboard');
-          } else if (userData.role === 'instructor') {
-            navigate('/instructor/dashboard');
-          } else {
-            navigate('/login');
-          }
+      const userData = await getUser(); 
+      console.log(userData.role);
 
-        } catch (error) {
-          console.error('Error logging in:', error);
-          if (error.response && error.response.status === 422) {
-            setError('Invalid email or password. Please try again.');
-          } else {
-            setError('An error occurred during login. Please try again.');
-          }
+      handleLogin(userData);
+      setTimeout(() => {
+        setIsLoading(false);
+        if (userData.role === 'admin') {
+          navigate('/admin/dashboard');
+        } else if (userData.role === 'student') {
+          navigate('/student/dashboard');
+        } else if (userData.role === 'instructor') {
+          navigate('/instructor/dashboard');
+        } else {
+          navigate('/login');
         }
-    };
+      }, 40);
 
-    const handleEmailFocus = () => {
-        setIsEmailFocused(true);
-    };
+    } catch (error) {
+      console.error('Error logging in:', error);
+      if (error.response && error.response.status === 422) {
+        setError('Invalid email or password. Please try again.');
+      } else {
+        setError('An error occurred during login. Please try again.');
+      }
+      setIsLoading(false);
+    }
+  };
 
-    const handleEmailBlur = () => {
-        setIsEmailFocused(false);
-    };
+  const handleEmailFocus = () => {
+    setIsEmailFocused(true);
+  };
 
-    const handlePasswordFocus = () => {
-        setIsPasswordFocused(true);
-    };
+  const handleEmailBlur = () => {
+    setIsEmailFocused(false);
+  };
 
-    const handlePasswordBlur = () => {
-        setIsPasswordFocused(false);
-    };
+  const handlePasswordFocus = () => {
+    setIsPasswordFocused(true);
+  };
 
-    return (
-        <div>
-            <div className="relative flex justify-center items-center h-screen  w-screen bg-cover bg-no-repeat" style={{ backgroundImage: 'linear-gradient(90deg, rgba(0, 0, 0, 0.65) 1.14%, rgba(0, 0, 0, 0.00) 40.45%), url("images/ustp.png")' }}>
-                <div className="absolute top-10 left-10 flex items-center" style={{ width: '50%' }}>
-                    <div className="w-24 h-7 bg-white mr-5">
-                        <div className="w-72 h-4 bg-slate-300"></div>
-                    </div>
-                </div>
-                <div className="max-w-md w-full bg-black bg-opacity-50 p-8 rounded-lg shadow-lg text-center ml-auto mr-10 ">
-                    <div className="text-white ">
-                        <img src="images/logo.PNG" alt="CampusHub Logo" className="mb-5 w-25 h-25 rounded-full bg-transparent relative mx-auto" />
-                        <h1 className="text-3xl mt-1 font-bold text-white">Login</h1>
-                    </div>
-                    {error && <p className="text-red-500 mt-2">{error}</p>}
-                    <form onSubmit={handleSubmit} className="mt-8 relative">
-                        <div className="relative">
-                            <FontAwesomeIcon icon={faUser} className={`absolute top-2 left-3 text-white ${isEmailFocused ? 'text-blue-500' : 'text-white'}`} />
-                            <label htmlFor="email" className="sr-only">Email</label>
-                            <input type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} onFocus={handleEmailFocus} onBlur={handleEmailBlur} name="email" required autoComplete="email" placeholder='Email' className="w-full h-10 pl-10 pr-4 mb-5 border-b-2 border-white bg-transparent text-white focus:outline-none focus:border-blue-500" />
-                        </div>
-                        <div className="relative">
-                            <FontAwesomeIcon icon={faLock} className={`absolute top-2 left-3 text-white ${isPasswordFocused ? 'text-blue-500' : 'text-white'}`} />
-                            <label htmlFor="password" className="sr-only">Password</label>
-                            <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} onFocus={handlePasswordFocus} onBlur={handlePasswordBlur} name="password" required autoComplete="current-password" placeholder='Password' className="w-full h-10 pl-10 pr-4 mb-5 border-b-2 border-white bg-transparent text-white focus:outline-none focus:border-blue-500" />
-                        </div>
-                        <div className='p-5'></div>
-                        <button type="submit" className="w-full mt-1 py-3 bg-slate-500 text-white rounded-md transition duration-300 ease-in-out hover:bg-blue-600 focus:outline-none">Login</button>
-                    </form>
-                    
-                    <div className="mt-4">
-                        <a href="#" className="block text-blue-100 text-sm">Forgot password?</a>
-                    </div>
+  const handlePasswordBlur = () => {
+    setIsPasswordFocused(false);
+  };
 
-                    <div className="mt-3 text-white text-sm">
-                        Don't have an account?<a href="/register" className="block text-blue-400">Register here</a>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-cover bg-no-repeat" style={{ backgroundImage: 'linear-gradient(90deg, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0)), url("src/assets/image3.jpg")' }}>
+      <div className="absolute top-10 left-10 flex items-center w-1/2">
+        <div className="w-24 h-7 bg-white mr-5">
+          <div className="w-72 h-4 bg-slate-300"></div>
         </div>
-    );
+      </div>
+      <div className="w-full max-w-sm bg-black bg-opacity-75 p-6 rounded-md shadow-md">
+        <div className="flex justify-center mb-5">
+          <img src="images/logo.PNG" alt="CampusHub Logo" className="w-20 h-20 rounded-full" />
+        </div>
+        <h1 className="text-3xl font-bold text-center text-white mb-6">Login</h1>
+        {error && <p className="text-red-500 text-center">{error}</p>}
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div className="relative">
+            <FontAwesomeIcon icon={faUser} className={`absolute top-1/2 left-3 transform -translate-y-1/2 ${isEmailFocused ? 'text-blue-500' : 'text-white'}`} />
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              onFocus={handleEmailFocus}
+              onBlur={handleEmailBlur}
+              required
+              autoComplete="email"
+              placeholder="Email"
+              className="w-full h-10 pl-10 pr-4 border-b-2 border-white bg-transparent text-white placeholder-gray-300 focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <div className="relative">
+            <FontAwesomeIcon icon={faLock} className={`absolute top-1/2 left-3 transform -translate-y-1/2 ${isPasswordFocused ? 'text-blue-500' : 'text-white'}`} />
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              onFocus={handlePasswordFocus}
+              onBlur={handlePasswordBlur}
+              required
+              autoComplete="current-password"
+              placeholder="Password"
+              className="w-full h-10 pl-10 pr-4 border-b-2 border-white bg-transparent text-white placeholder-gray-300 focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <button type="submit" className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none transition duration-300">
+            {isLoading ? (
+              <div className="flex justify-center items-center space-x-2">
+                {[...Array(4)].map((_, i) => (
+                  <div key={i} className="w-2 h-2 bg-black rounded-full opacity-75 animate-bounce" style={{ animationDelay: `${i * 0.1}s` }}></div>
+                ))}
+              </div>
+            ) : (
+              'Login'
+            )}
+          </button>
+        </form>
+        <div className="mt-4 text-center">
+          <a href="#" className="text-blue-100 text-sm">Forgot password?</a>
+        </div>
+        <div className="mt-3 text-center text-white text-sm">
+          Don't have an account? <a href="/register" className="text-blue-400">Register here</a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginForm;
--- front_end/src/components/RegisterForm.jsx
+++ front_end/src/components/RegisterForm.jsx
@@ -1,9 +1,11 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import axiosInstance from '../axios';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faEnvelope, faLock, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { register } from '../auth';
+import './animations.css';
+
 
 const RegisterForm = () => {
   const [name, setName] = useState('');
@@ -12,7 +14,10 @@ const RegisterForm = () => {
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
   const [role, setRole] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
-  const [successMessage, setSuccessMessage] = useState('')
+  const [successMessage, setSuccessMessage] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function fetchCsrfToken() {
@@ -29,24 +34,29 @@ const RegisterForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setErrorMessage('');
+    setIsLoading(true);
 
     if (email !== email.toLowerCase()) {
       setErrorMessage('Email must be in lowercase.');
+      setIsLoading(false);
       return;
     }
 
     if (password !== passwordConfirmation) {
       setErrorMessage('Passwords do not match.');
+      setIsLoading(false);
       return;
     }
 
     if (password.length < 8) {
       setErrorMessage('Password must be at least 8 characters long.');
+      setIsLoading(false);
       return;
     }
 
     if (!role) {
-      setErrorMessage('You must pick a role in LIFE MYGHAD.');
+      setErrorMessage('You must pick a role.');
+      setIsLoading(false);
       return;
     }
 
@@ -54,108 +64,133 @@ const RegisterForm = () => {
       await register(name, email, password, passwordConfirmation, role);
       setSuccessMessage('Registration Successful!');
       setErrorMessage('');
-
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 40);
     } catch (error) {
       if (error.response && error.response.status === 422) {
         setErrorMessage('Invalid data. Please check your input.');
-
       } else if (error.response && error.response.data) {
         setErrorMessage(error.response.data.message || 'An error occurred during registration.');
-
       } else {
         setErrorMessage('An error occurred. Please try again later.');
-
       }
-
       console.error('Error registering:', error);
+      setIsLoading(false);
     }
   };
 
   return (
-    <div>
-      <div className="relative flex justify-center items-center h-screen w-screen bg-cover bg-no-repeat" style={{ backgroundImage: 'linear-gradient(90deg, rgba(0, 0, 0, 0.65) 1.14%, rgba(0, 0, 0, 0.00) 40.45%), url("images/ustp.png")' }}>
-        <div className="mx-auto p-10 md:p-20 m-5 md:m-10 bg-black bg-opacity-90 shadow-md rounded-md h-auto md:h-auto lg:h-auto max-h-screen">
-          <div className="flex justify-center">
-            <img src="images/logo.PNG" alt="CampusHub Logo" className="mb-5 w-50 h-50 rounded-full relative" />
-          </div>
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <h1 className="text-3xl font-bold text-white text-center mb-6">Register</h1>
-            {errorMessage && <p className="relative text-red-500 text-center text-[14px]">{errorMessage}</p>}
-            {successMessage && <p className="relative text-green-500 text-center text-[14px]">{successMessage}</p>}
-            <div className="flex space-x-4">
-              <div className="flex flex-col w-1/2 relative">
-                <input
-                  id="name"
-                  name="name"
-                  type="text"
-                  className="w-full h-10 mb-5 border-b-2 border-white bg-transparent text-white placeholder-gray-200 placeholder-opacity-10 focus:outline-none focus:border-blue-500 pl-10"
-                  placeholder="Name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  required
-                />
-                <FontAwesomeIcon icon={faUser} className="absolute top-3 left-3 text-white" />
-              </div>
-              <div className="flex flex-col w-1/2 relative">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  className="w-full h-10 mb-5 border-b-2 border-white bg-transparent text-white placeholder-gray-200 placeholder-opacity-10 focus:outline-none focus:border-blue-500 pl-10"
-                  placeholder="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-                <FontAwesomeIcon icon={faEnvelope} className="absolute top-3 left-3 text-white" />
-              </div>
-            </div>
-            <div className="flex space-x-4">
-              <div className="flex flex-col w-1/2 relative">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  className="w-full h-10 mb-5 border-b-2 border-white bg-transparent text-white placeholder-gray-300 placeholder-opacity-10 focus:outline-none focus:border-blue-500 pl-10"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-                <FontAwesomeIcon icon={faLock} className="absolute top-3 left-3 text-white" />
-              </div>
-              <div className="flex flex-col w-1/2 relative">
-                <input
-                  id="confirmPassword"
-                  name="confirmPassword"
-                  type="password"
-                  className="w-full h-10 mb-5 border-b-2 border-white bg-transparent text-white placeholder-gray-300 placeholder-opacity-10 bg-opacity-10 focus:outline-none focus:border-blue-500 pl-10"
-                  placeholder="Confirm Password"
-                  value={passwordConfirmation}
-                  onChange={(e) => setPasswordConfirmation(e.target.value)}
-                  required
-                />
-                <FontAwesomeIcon icon={faLock} className="absolute top-3 left-3 text-white" />
-              </div>
-              <div className="relative activeflex items-center mb-4 mt-[20px]">
-                  <input type="radio" value="student" checked={role === 'student'} onChange={() => setRole('student')} className="mr-2" text-white />
-                  <label className="text-white">Student</label>
-                  <input type="radio" value="admin" checked={role === 'admin'} onChange={() => setRole('admin')} className="ml-4 mr-2 text-white" />
-                  <label className="text-white">Admin</label>
-                  <input type="radio" value="instructor" checked={role === 'instructor'} onChange={() => setRole('instructor')} className="ml-4 mr-2 text-white" />
-                  <label className="text-white">Instructor</label> 
-              </div>
-            </div>
-            <button
-              type="submit"
-              className="w-full h-10 mb-5 border-b-2  bg-slate-500 text-white transition duration-300 ease-in-out hover:bg-blue-600 focus:outline-none">
-              Register
-            </button>
-            <div className="mt-3 text-center">
-              Already have an account?<a href="/login" className="block text-white text-sm">Login here</a>
-            </div>
-          </form>
+    <div className="flex justify-center items-center min-h-screen bg-cover bg-no-repeat" style={{ backgroundImage: 'linear-gradient(90deg, rgba(0, 0, 0, 0.65) 1.14%, rgba(0, 0, 0, 0.00) 40.45%), url("src/assets/image3.jpg")' }}>
+      <div className="w-full max-w-md p-6 bg-black bg-opacity-90 shadow-md rounded-md mx-4">
+        <div className="flex justify-center mb-5">
+          <img src="images/logo.PNG" alt="CampusHub Logo" className="w-24 h-24 rounded-full" />
         </div>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <h1 className="text-3xl font-bold text-white text-center mb-6">Register</h1>
+          {errorMessage && <p className="text-red-500 text-center text-[14px]">{errorMessage}</p>}
+          {successMessage && <p className="text-green-500 text-center text-[14px]">{successMessage}</p>}
+          <div className="flex flex-col space-y-4">
+            <div className="relative">
+              <input
+                id="name"
+                name="name"
+                type="text"
+                className="w-full h-10 border-b-2 border-white bg-transparent text-white placeholder-gray-200 focus:outline-none focus:border-blue-500 pl-10"
+                placeholder="Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+              <FontAwesomeIcon icon={faUser} className="absolute top-3 left-3 text-white" />
+            </div>
+            <div className="relative">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                className="w-full h-10 border-b-2 border-white bg-transparent text-white placeholder-gray-200 focus:outline-none focus:border-blue-500 pl-10"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <FontAwesomeIcon icon={faEnvelope} className="absolute top-3 left-3 text-white" />
+            </div>
+            <div className="relative">
+              <input
+                id="password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                className="w-full h-10 border-b-2 border-white bg-transparent text-white placeholder-gray-300 focus:outline-none focus:border-blue-500 pl-10 pr-10"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <FontAwesomeIcon icon={faLock} className="absolute top-3 left-3 text-white" />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute top-1/2 right-1 transform -translate-y-1/2 text-white focus:outline-none border-none"
+                style={{ background: 'transparent' }}
+              >
+                <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash} />
+              </button>
+            </div>
+            <div className="relative">
+              <input
+                id="confirmPassword"
+                name="confirmPassword"
+                type={showConfirmPassword ? "text" : "password"}
+                className="w-full h-10 border-b-2 border-white bg-transparent text-white placeholder-gray-300 focus:outline-none focus:border-blue-500 pl-10 pr-10"
+                placeholder="Confirm Password"
+                value={passwordConfirmation}
+                onChange={(e) => setPasswordConfirmation(e.target.value)}
+                required
+              />
+              <FontAwesomeIcon icon={faLock} className="absolute top-3 left-3 text-white" />
+              <button
+                type="button"
+                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                className="absolute top-1/2 right-1 transform -translate-y-1/2 text-white focus:outline-none border-none"
+                style={{ background: 'transparent' }}
+              >
+                <FontAwesomeIcon icon={showConfirmPassword ? faEye : faEyeSlash} />
+              </button>
+            </div>
+            <div className="flex items-center space-x-4 text-white">
+              <label className="flex items-center">
+                <input type="radio" value="student" checked={role === 'student'} onChange={() => setRole('student')} className="mr-2" />
+                Student
+              </label>
+              <label className="flex items-center">
+                <input type="radio" value="admin" checked={role === 'admin'} onChange={() => setRole('admin')} className="mr-2" />
+                Admin
+              </label>
+              <label className="flex items-center">
+                <input type="radio" value="instructor" checked={role === 'instructor'} onChange={() => setRole('instructor')} className="mr-2" />
+                Instructor
+              </label>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="w-full h-10 bg-blue-500 text-white transition duration-300 ease-in-out hover:bg-blue-600 focus:outline-none">
+            {isLoading ? (
+              <div className="flex justify-center items-center space-x-2">
+                {[...Array(4)].map((_, i) => (
+                  <div key={i} className="w-2 h-2 bg-black rounded-full opacity-75 animate-bounce" style={{ animationDelay: `${i * 0.1}s` }}></div>
+                ))}
+              </div>
+            ) : (
+              'Register'
+            )}
+          </button>
+          <div className="mt-3 text-center text-white">
+            Already have an account? <a href="/login" className="text-white underline text-sm">Login here</a>
+          </div>
+        </form>
       </div>
     </div>
   );
